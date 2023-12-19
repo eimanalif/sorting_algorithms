b11ca179dbba206584e33ec2afae3c6270825144
eimanalif <eiman.fadlelmowla@gmail.com>
2023-12-19 03:06:58 -0800

    2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,3 +15,25 @@
 
 void selection_sort(int *array, size_t size)
 {
+	int x;
+	size_t i, j, k;
+
+	if (!array || !size)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = size - 1, k = i + 1; j > i; j--)
+		{
+			if  (array[j] < array[k])
+				k = j;
+		}
+		if  (array[i] > array[k])
+		{
+			x = array[i];
+			array[i] = array[k];
+			array[k] = x;
+			print_array(array, size);
+		}
+	}
+}
