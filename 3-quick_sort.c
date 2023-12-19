39b982ba0f1f774767ee1c8b54c06e243683962d
eimanalif <eiman.fadlelmowla@gmail.com>
2023-12-19 04:08:15 -0800

    2

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -23,7 +23,7 @@ void swap(int *array, size_t size, int *x, int *y)
 		print_array((const int *)array, size);
 	}
 }
-	
+
 
 
 
@@ -83,5 +83,5 @@ void quick_sort(int *array, size_t size)
 	if (!array || !size)
 		return;
 
-	quicksort(array, size, 0, size-1);
-}	
+	quicksort(array, size, 0, size - 1);
+}
